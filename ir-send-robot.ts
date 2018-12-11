3a28bc12fd3095c8f3b19d6cc9bff379098bad87
durjaya@gmail.com <durjaya123>
2018-12-11 13:54:28 +0800

    IR coding

--- ir-send-robot.ts
+++ ir-send-robot.ts
@@ -1,5 +1,6 @@
 // slow down factor for demo, at about 100 or 200 times slower
 let FACTOR = 1000
+let value  = 0
 
 //function ledOn(d: number) {
 //    let r = d * FACTOR;
@@ -13,96 +14,435 @@ let FACTOR = 1000
 
 function ledOn(d: number) {
     let r = d * FACTOR;
+    value = r
     while (r > 26) {
         basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
         control.waitMicros(2);
         basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
         r = r - 26;
     }
 }
 
 
 function ledOff(d: number) {
+    value =d 
     control.waitMicros(d * FACTOR);
 }
 
-//send 8 bit
-function send(code: number) {
-    for (let i = 15; i > -1; i--) {
-        if (1 << i & code) {
-            ledOn(1);
-        } else {
-            ledOff(1);
-        }
-    }
-}
 
-// send 32 bit with appropriate start and end
+// Stop
 function stop() {
 
     //1
     ledOn(6000);
-
     //2
     ledOff(600);
-
     //3
     ledOn(600);
-
     //4
     ledOff(1500);
-
     //5
     ledOn(1500);
-
     //6
     ledOff(600);
-
     //7
     ledOn(1500);
-
     //8
     ledOff(600);
-
     //9
     ledOn(1500);
-
     //10
     ledOff(600);
-
     //11
     ledOn(600);
-
     //12
     ledOff(1500);
-
     //13
     ledOn(1500);
-
     //14
     ledOff(600);
-
     //15
     ledOn(1500);
-
     //16
     ledOff(600);
-
     //17
     ledOn(1500);
 }
+
+//Dance
+function dance(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+}
+
+function volumePlus(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500); 
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500); 
+    ledOn(600);
+    ledOff(1500); 
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+}
+
+function volumeSub(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500); 
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500); 
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500); 
+    ledOn(600);
+}
+
+function moveForward(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+}
+
+function moveBack(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+}
+
+function lookLeft(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+}
+
+function lookRight(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+}
+
+function slipForward(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+}
+
+function slipBack(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+}
+
+function turnLeft(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+}
+
+function turnRight(){
+    ledOn(5950);
+    ledOff(500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+}
+
+function math(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+}
+
+function science(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+}
+
+function english(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+}
+
+function voiceControl(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+}
+
+function programming(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+}
+
+function energeTransform(){
+    ledOn(6000);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+}
+
+function music(){
+    ledOn(5950);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(1500);
+    ledOff(600);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(600);
+    ledOff(1500);
+    ledOn(1500);
+}
+
+
 
 // Button A sends code
 input.onButtonPressed(Button.A, () => {
@@ -111,9 +451,7 @@ input.onButtonPressed(Button.A, () => {
 })
 
 
-/**
-basic.forever(function () {
-    serial.writeValue("x", 0)
-})
 
-**/
+basic.forever(function () {
+    serial.writeValue("x", r)
+})
